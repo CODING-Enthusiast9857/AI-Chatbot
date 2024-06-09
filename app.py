dcb5b343f1f7449e41e78cbc9c12f1a404074962
Madhavi Sonawane <madhavison06@gmail.com>
2024-06-09 19:37:08 +0530

    CORS support

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS 
 from chat import get_response
 
 app = Flask(__name__)
+CORS(app)
 
 # @app.route("/", methods=["GET"])
 @app.get("/")
