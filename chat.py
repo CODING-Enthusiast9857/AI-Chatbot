7daf2fe1e07243078d4d087429d3b47ba6eed663
Madhavi Sonawane <madhavison06@gmail.com>
2024-06-09 19:52:49 +0530

    some chnages done

--- chat.py
+++ chat.py
@@ -23,7 +23,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Sam"
+bot_name = "Sammy"
 
 def get_response(msg):
     sentence = tokenize(msg)
