81d461d52668f5c5563990c43a6daf5d16c70e20
Madhavi Sonawane <madhavison06@gmail.com>
2024-06-09 20:09:04 +0530

    newly trained data

--- train.py
+++ train.py
@@ -51,10 +51,10 @@ y_train = np.array(y_train)
 
 # Hyper-parameters 
 num_epochs = 1000
-batch_size = 8
+batch_size = 16
 learning_rate = 0.001
 input_size = len(X_train[0])
-hidden_size = 8
+hidden_size = 16
 output_size = len(tags)
 
 class ChatDataset(Dataset):
