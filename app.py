6aaabbc98252c01b041cdf199d9bde56d60da137
Madhavi Sonawane <madhavison06@gmail.com>
2024-06-10 14:20:57 +0530

    app updated

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS 
 from chat import get_response
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/", methods=["GET"])
 # @app.get("/")
